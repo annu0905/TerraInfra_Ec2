1297359cec0bfc90ea5980ed5f7a5fe01b1628d3
annu <anusharmaa09@gmail.com>
2022-01-14 16:16:33 +0530

    removed provisioners

--- ec2.tf
+++ ec2.tf
@@ -8,18 +8,5 @@ resource "aws_instance" "my_ec2"{
   ami = "ami-0af25d0df86db00c1"
   instance_type= "t2.micro"
   key_name= "terraform_key"
-
-  provisioner "remote-exec"{
-    inline=[
-      "sudo yum install -y httpd",
-      "sudo systemctl start httpd"
-    ]
-
-    connection {
-      type= "ssh"
-      user= "ec2-user"
-      private_key= file("./terraform_key.pem")
-      host= self.public_ip
-    }
-  }
 }
+
